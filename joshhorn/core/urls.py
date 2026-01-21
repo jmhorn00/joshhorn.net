f0387e0650bac52f9689c5e9efc11f2ff166dfc5
josh1115 <79900187+josh1115@users.noreply.github.com>
2026-01-20 22:05:33 -0600

    Feat: A start to a navbar adn moble navbar

--- joshhorn/core/urls.py
+++ joshhorn/core/urls.py
@@ -7,6 +7,10 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
+    
     
    
 ]
--- joshhorn/core/views.py
+++ joshhorn/core/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/pages/home.html')
+
+def about(request):
+    return render(request, 'core/pages/about.html')
+def projects(request):
+    return render(request, 'core/pages/projects.html')
+def contact(request):
+    return render(request, 'core/pages/contact.html')
